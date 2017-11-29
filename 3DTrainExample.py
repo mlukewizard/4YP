001a2d3e60cf902e536e13169f551f8814afc1a1
mlukewizard <lukemarkham1383@gmail.com>
2017-11-29 15:48:11 +0000

    Adapted 3DTrainExample to work with separate training and validation files

--- 3DTrainExample.py
+++ 3DTrainExample.py
@@ -21,118 +21,122 @@ def my_loss(y_true, y_pred):
 #    return K.mean(K.binary_crossentropy(y_true[:,2,:,:,:], y_pred[:,2,:,:,:], axis=-1)
 
 validationArrayPath = '/home/lukemarkham1383/trainEnvironment/npArrays/validation/'
-trainingArrayPath = '/home/lukemarkham1383/trainEnvironment/npArrays/training/'
- 
-for k in range(10):  # 50 means training for 50 epochs
-    img_measure_file = '3DAugment001-002PatientNS_Original.npy'
-    bm_measure_file = '3DAugment001-002PatientNS_InnerBinary.npy'
-    img_measure = np.load(os.path.join(trainingArrayPath, img_measure_file))
-    bm_measure = np.load(os.path.join(trainingArrayPath, bm_measure_file)) / 255  # Converting to binary
+trainingArrayDicomPath = '/home/lukemarkham1383/trainEnvironment/npArrays/training/originals/'
+trainingArrayBinaryPath = '/home/lukemarkham1383/trainEnvironment/npArrays/training/binary/'
 
-    img_test_file = 'nonAugmentPatientNS_Original.npy'
-    bm_test_file = 'nonAugmentPatientNS_Binary.npy'
-    img_test = np.load(os.path.join(file_dict, img_test_file))
-    bm_test = np.load(os.path.join(file_dict, bm_test_file)) / 255
+img_test_file = '3DnonAugmentPatientRR_Original.npy'
+bm_test_file = '3DNonAugmentPatientRR_InnerBinary.npy'
+img_test = np.load(os.path.join(validationArrayPath, img_test_file))
+bm_test = np.load(os.path.join(validationArrayPath, bm_test_file)) / 255
 
-    testSplit = img_test.shape[0]/(img_test.shape[0]+img_measure.shape[0])
-    img_train = np.concatenate((img_measure, img_test))
-    bm_train = np.concatenate((bm_measure, bm_test))
+fileList = sorted(os.listdir(trainingArrayDicomPath))
 
-    model_folder = '/home/lukemarkham1383/trainEnvironment/models'  # Change this
-    model_list = os.listdir(model_folder)  # Checking if there is an existing model
-    if model_list.__len__() == 0:  # Creating a new model if empty
+for k in range(10): #runs for the len(filelist) * the number in range epochs
+    for filename in fileList:
+        img_measure_file = filename
+        split1 = filename.split('Original')[0]
+        bm_measure_file = split1 + 'Binary.npy'
+        img_measure = np.load(os.path.join(trainingArrayPath, img_measure_file))
+        bm_measure = np.load(os.path.join(trainingArrayPath, bm_measure_file)) / 255  # Converting to binary
 
-        inputs = Input((5, 256, 256, 1))
+        testSplit = img_test.shape[0]/(img_test.shape[0]+img_measure.shape[0])
+        img_train = np.concatenate((img_measure, img_test))
+        bm_train = np.concatenate((bm_measure, bm_test))
 
-        conv1 = TimeDistributed(BatchNormalization())(inputs)
-        conv1 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv1)
-        conv1 = TimeDistributed(BatchNormalization())(conv1)
-        conv1 = TimeDistributed(Dropout(0.5))(conv1)
-        conv1 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv1)
-        pool1 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv1)
+        model_folder = '/home/lukemarkham1383/trainEnvironment/models'  # Change this
+        model_list = os.listdir(model_folder)  # Checking if there is an existing model
+        if model_list.__len__() == 0:  # Creating a new model if empty
 
-        conv2 = TimeDistributed(BatchNormalization())(pool1)
-        conv2 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv2)
-        conv2 = TimeDistributed(BatchNormalization())(conv2)
-        conv2 = TimeDistributed(Dropout(0.5))(conv2)
-        conv2 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv2)
-        pool2 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv2)
+            inputs = Input((5, 256, 256, 1))
 
-        conv3 = TimeDistributed(BatchNormalization())(pool2)
-        conv3 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv3)
-        conv3 = TimeDistributed(BatchNormalization())(conv3)
-        conv3 = TimeDistributed(Dropout(0.5))(conv3)
-        conv3 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv3)
-        pool3 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv3)
+            conv1 = TimeDistributed(BatchNormalization())(inputs)
+            conv1 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv1)
+            conv1 = TimeDistributed(BatchNormalization())(conv1)
+            conv1 = TimeDistributed(Dropout(0.5))(conv1)
+            conv1 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv1)
+            pool1 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv1)
 
-        conv4 = TimeDistributed(BatchNormalization())(pool3)
-        conv4 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv4)
-        conv4 = TimeDistributed(BatchNormalization())(conv4)
-        conv4 = TimeDistributed(Dropout(0.5))(conv4)
-        conv4 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv4)
-        pool4 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv4)
+            conv2 = TimeDistributed(BatchNormalization())(pool1)
+            conv2 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv2)
+            conv2 = TimeDistributed(BatchNormalization())(conv2)
+            conv2 = TimeDistributed(Dropout(0.5))(conv2)
+            conv2 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv2)
+            pool2 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv2)
 
-        myLSTM = Bidirectional(ConvLSTM2D(512, (3, 3), activation='relu', padding='same', return_sequences=True))(pool4)
+            conv3 = TimeDistributed(BatchNormalization())(pool2)
+            conv3 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv3)
+            conv3 = TimeDistributed(BatchNormalization())(conv3)
+            conv3 = TimeDistributed(Dropout(0.5))(conv3)
+            conv3 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv3)
+            pool3 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv3)
 
-        up6 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(myLSTM), conv4], axis=4)
-        conv6 = TimeDistributed(BatchNormalization())(up6)
-        conv6 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv6)
-        conv6 = TimeDistributed(BatchNormalization())(conv6)
-        conv6 = TimeDistributed(Dropout(0.5))(conv6)
-        conv6 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv6)
+            conv4 = TimeDistributed(BatchNormalization())(pool3)
+            conv4 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv4)
+            conv4 = TimeDistributed(BatchNormalization())(conv4)
+            conv4 = TimeDistributed(Dropout(0.5))(conv4)
+            conv4 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv4)
+            pool4 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv4)
 
-        up7 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv6), conv3], axis=4)
-        conv7 = TimeDistributed(BatchNormalization())(up7)
-        conv7 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv7)
-        conv7 = TimeDistributed(BatchNormalization())(conv7)
-        conv7 = TimeDistributed(Dropout(0.5))(conv7)
-        conv7 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv7)
+            myLSTM = Bidirectional(ConvLSTM2D(512, (3, 3), activation='relu', padding='same', return_sequences=True))(pool4)
 
-        up8 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv7), conv2], axis=4)
-        conv8 = TimeDistributed(BatchNormalization())(up8)
-        conv8 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv8)
-        conv8 = TimeDistributed(BatchNormalization())(conv8)
-        conv8 = TimeDistributed(Dropout(0.5))(conv8)
-        conv8 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv8)
+            up6 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(myLSTM), conv4], axis=4)
+            conv6 = TimeDistributed(BatchNormalization())(up6)
+            conv6 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv6)
+            conv6 = TimeDistributed(BatchNormalization())(conv6)
+            conv6 = TimeDistributed(Dropout(0.5))(conv6)
+            conv6 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv6)
 
-        up9 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv8), conv1], axis=4)
-        conv9 = TimeDistributed(BatchNormalization())(up9)
-        conv9 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv9)
-        conv9 = TimeDistributed(BatchNormalization())(conv9)
-        conv9 = TimeDistributed(Dropout(0.5))(conv9)
-        conv9 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv9)
+            up7 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv6), conv3], axis=4)
+            conv7 = TimeDistributed(BatchNormalization())(up7)
+            conv7 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv7)
+            conv7 = TimeDistributed(BatchNormalization())(conv7)
+            conv7 = TimeDistributed(Dropout(0.5))(conv7)
+            conv7 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv7)
 
-        conv10 = TimeDistributed(BatchNormalization())(conv9)
-        conv10 = TimeDistributed(Dropout(0.5))(conv10)
-        conv10 = TimeDistributed(Conv2D(2, (1, 1), activation='sigmoid'))(conv10)
+            up8 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv7), conv2], axis=4)
+            conv8 = TimeDistributed(BatchNormalization())(up8)
+            conv8 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv8)
+            conv8 = TimeDistributed(BatchNormalization())(conv8)
+            conv8 = TimeDistributed(Dropout(0.5))(conv8)
+            conv8 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv8)
 
-        model = Model(inputs=[inputs], outputs=[conv10])
-	
-	epoch_number = 0
+            up9 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv8), conv1], axis=4)
+            conv9 = TimeDistributed(BatchNormalization())(up9)
+            conv9 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv9)
+            conv9 = TimeDistributed(BatchNormalization())(conv9)
+            conv9 = TimeDistributed(Dropout(0.5))(conv9)
+            conv9 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv9)
 
-    else:
+            conv10 = TimeDistributed(BatchNormalization())(conv9)
+            conv10 = TimeDistributed(Dropout(0.5))(conv10)
+            conv10 = TimeDistributed(Conv2D(2, (1, 1), activation='sigmoid'))(conv10)
 
-        currentMax = 0
-        for fn in model_list:
-            epoch_number = int(fn.split('weights.')[1].split('-')[0])
-            if epoch_number > currentMax:
-                currentMax = epoch_number
-                model_file = fn
-        epoch_number = int(model_file.split('weights.')[1].split('-')[0])
-        f_model = h5py.File(os.path.join(model_folder, model_file), 'r+')
-        if 'optimizer_weights' in f_model:
-            del f_model['optimizer_weights']
-        f_model.close()
-        model = load_model(os.path.join(model_folder, model_file))
-        print('Using model number ' + str(epoch_number))
-	
-    model.summary()
-    model.compile(optimizer=Adam(lr=1e-3), loss=losses.binary_crossentropy)
+            model = Model(inputs=[inputs], outputs=[conv10])
 
-    model_check_file = os.path.join(model_folder, 'weights.{epoch:02d}-{loss:.2f}.h5')
+            epoch_number = 0
 
-    model_checkpoint = ModelCheckpoint(model_check_file, monitor='val_loss', save_best_only=False)
+        else:
+            currentMax = 0
+            for fn in model_list:
+                epoch_number = int(fn.split('weights.')[1].split('-')[0])
+                if epoch_number > currentMax:
+                    currentMax = epoch_number
+                    model_file = fn
+            epoch_number = int(model_file.split('weights.')[1].split('-')[0])
+            f_model = h5py.File(os.path.join(model_folder, model_file), 'r+')
+            if 'optimizer_weights' in f_model:
+                del f_model['optimizer_weights']
+            f_model.close()
+            model = load_model(os.path.join(model_folder, model_file))
+            print('Using model number ' + str(epoch_number))
 
-    history = model.fit(img_train, bm_train, batch_size=4, initial_epoch=epoch_number, epochs=epoch_number + 10,
-                        verbose=1, shuffle=True, validation_split=0.5,
-                        callbacks=[model_checkpoint])
+        model.summary()
+        model.compile(optimizer=Adam(lr=1e-3), loss=losses.binary_crossentropy)
+
+        model_check_file = os.path.join(model_folder, 'weights.{epoch:02d}-{loss:.2f}.h5')
+
+        model_checkpoint = ModelCheckpoint(model_check_file, monitor='val_loss', save_best_only=False)
+
+        history = model.fit(img_train, bm_train, batch_size=4, initial_epoch=epoch_number, epochs=epoch_number + 1,
+                            verbose=1, shuffle=True, validation_split=testSplit,
+                            callbacks=[model_checkpoint])
