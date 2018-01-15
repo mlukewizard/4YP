4e6c3541958369900b13f990ae70eab68ac4ce77
mlukewizard <lukemarkham1383@instance-6.c.silicon-vertex-182720.internal>
2018-01-15 19:35:50 +0000

    Changed network architecture

--- TrainExample3D.py
+++ TrainExample3D.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from __future__ import division
 from keras.callbacks import ModelCheckpoint
 from keras.models import Model, load_model
-from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, ConvLSTM2D, LSTM, TimeDistributed, Bidirectional, Dropout, BatchNormalization
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, ConvLSTM2D, LSTM, TimeDistributed, Bidirectional, Dropout, BatchNormalization, Activation
 from keras.optimizers import Adam
 from keras import losses
 import numpy as np
@@ -90,71 +90,65 @@ for k in range(10):
     if model_list.__len__() == 0:  # Creating a new model if empty
 
         inputs = Input((5, 256, 256, 1))
-
-        conv1 = TimeDistributed(BatchNormalization())(inputs)
-        conv1 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv1)
-        conv1 = TimeDistributed(BatchNormalization())(conv1)
-        conv1 = TimeDistributed(Dropout(0.5))(conv1)
-        conv1 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv1)
+	
+	conv1 = TimeDistributed(BatchNormalization())(inputs)
+        conv1 = TimeDistributed(Conv2D(32, (3, 3), padding='same'))(conv1)
+	conv1 = TimeDistributed(BatchNormalization())(conv1)
+	conv1 = TimeDistributed(Activation('relu'))(conv1)
+	conv1 = TimeDistributed(Dropout(0.2))(conv1)
         pool1 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv1)
+	pool1 = TimeDistributed(Dropout(0.2))(pool1)	
 
-        conv2 = TimeDistributed(BatchNormalization())(pool1)
-        conv2 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv2)
+        conv2 = TimeDistributed(Conv2D(64, (3, 3), padding='same'))(pool1)
         conv2 = TimeDistributed(BatchNormalization())(conv2)
-        conv2 = TimeDistributed(Dropout(0.5))(conv2)
-        conv2 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv2)
+	conv2 = TimeDistributed(Activation('relu'))(conv2)
+        conv2 = TimeDistributed(Dropout(0.2))(conv2)
         pool2 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv2)
+	pool2 = TimeDistributed(Dropout(0.2))(pool2)
 
-        conv3 = TimeDistributed(BatchNormalization())(pool2)
-        conv3 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv3)
-        #conv3 = TimeDistributed(BatchNormalization())(conv3)
-        #conv3 = TimeDistributed(Dropout(0.5))(conv3)
-        #conv3 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv3)
+        conv3 = TimeDistributed(Conv2D(128, (3, 3), padding='same'))(pool2)
+        conv3 = TimeDistributed(BatchNormalization())(conv3)
+	conv3 = TimeDistributed(Activation('relu'))(conv3)
+        conv3 = TimeDistributed(Dropout(0.2))(conv3)
         pool3 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv3)
+	pool3 = TimeDistributed(Dropout(0.2))(pool3)
 
-        conv4 = TimeDistributed(BatchNormalization())(pool3)
-        conv4 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv4)
-        #conv4 = TimeDistributed(BatchNormalization())(conv4)
-        #conv4 = TimeDistributed(Dropout(0.5))(conv4)
-        #conv4 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv4)
+        conv4 = TimeDistributed(Conv2D(256, (3, 3), padding='same'))(pool3)
+        conv4 = TimeDistributed(BatchNormalization())(conv4)
+        conv4 = TimeDistributed(Activation('relu'))(conv4)
+        conv4 = TimeDistributed(Dropout(0.2))(conv4)
         pool4 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv4)
+        pool4 = TimeDistributed(Dropout(0.2))(pool4)
 
         myLSTM = Bidirectional(ConvLSTM2D(512, (3, 3), activation='relu', padding='same', return_sequences=True))(pool4)
 
-        #myLSTM = Bidirectional(ConvLSTM2D(256, (3, 3), activation='relu', padding='same', return_sequences=True))(pool3)
-
         up6 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(myLSTM), conv4], axis=4)
-        conv6 = TimeDistributed(BatchNormalization())(up6)
-        conv6 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv6)
-        #conv6 = TimeDistributed(BatchNormalization())(conv6)
-        #conv6 = TimeDistributed(Dropout(0.5))(conv6)
-        #conv6 = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same'))(conv6)
+        conv6 = TimeDistributed(Conv2D(256, (3, 3), padding='same'))(up6)
+	conv6 = TimeDistributed(BatchNormalization())(conv6)
+	conv6 = TimeDistributed(Activation('relu'))(conv6)
+        conv6 = TimeDistributed(Dropout(0.2))(conv6)
 
         up7 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv6), conv3], axis=4)
-        #up7 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(myLSTM), conv3], axis=4)
-        conv7 = TimeDistributed(BatchNormalization())(up7)
-        conv7 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv7)
-        #conv7 = TimeDistributed(BatchNormalization())(conv7)
-        #conv7 = TimeDistributed(Dropout(0.5))(conv7)
-        #conv7 = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same'))(conv7)
+        conv7 = TimeDistributed(Conv2D(128, (3, 3), padding='same'))(up7)
+        conv7 = TimeDistributed(BatchNormalization())(conv7)
+        conv7 = TimeDistributed(Activation('relu'))(conv7)
+        conv7 = TimeDistributed(Dropout(0.2))(conv7)
 
-        up8 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv7), conv2], axis=4)
-        conv8 = TimeDistributed(BatchNormalization())(up8)
-        conv8 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv8)
+	up8 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv7), conv2], axis=4)
+        conv8 = TimeDistributed(Conv2D(64, (3, 3), padding='same'))(up8)
         conv8 = TimeDistributed(BatchNormalization())(conv8)
-        conv8 = TimeDistributed(Dropout(0.5))(conv8)
-        conv8 = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same'))(conv8)
+        conv8 = TimeDistributed(Activation('relu'))(conv8)
+        conv8 = TimeDistributed(Dropout(0.2))(conv8)
 
-        up9 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv8), conv1], axis=4)
-        conv9 = TimeDistributed(BatchNormalization())(up9)
-        conv9 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv9)
+	up9 = concatenate([TimeDistributed(UpSampling2D(size=(2, 2)))(conv8), conv1], axis=4)
+        conv9 = TimeDistributed(Conv2D(32, (3, 3), padding='same'))(up9)
         conv9 = TimeDistributed(BatchNormalization())(conv9)
-        conv9 = TimeDistributed(Dropout(0.5))(conv9)
-        conv9 = TimeDistributed(Conv2D(32, (3, 3), activation='relu', padding='same'))(conv9)
+        conv9 = TimeDistributed(Activation('relu'))(conv9)
+        conv9 = TimeDistributed(Dropout(0.2))(conv9)
 
-        conv10 = TimeDistributed(BatchNormalization())(conv9)
-        conv10 = TimeDistributed(Dropout(0.5))(conv10)
-        conv10 = TimeDistributed(Conv2D(2, (1, 1), activation='sigmoid'))(conv10)
+        conv10 = TimeDistributed(Conv2D(2, (1, 1)))(conv9)
+	conv10 = TimeDistributed(BatchNormalization())(conv10)
+	conv10 = TimeDistributed(Activation('sigmoid'))(conv10)
 
         model = Model(inputs=[inputs], outputs=[conv10])
 
